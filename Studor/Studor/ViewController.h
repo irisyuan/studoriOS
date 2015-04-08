f9af1063d8cef6129cf7aeedd1ded720c7866d59
Omid Keypour <ok455@nyu.edu>
2015-04-08 16:06:03 -0400

    init commit

---
+++ Studor/Studor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Studor
+//
+//  Created by Omid Keypour on 4/8/15.
+//  Copyright (c) 2015 Omid Keypour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Studor/Studor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Studor
+//
+//  Created by Omid Keypour on 4/8/15.
+//  Copyright (c) 2015 Omid Keypour. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
