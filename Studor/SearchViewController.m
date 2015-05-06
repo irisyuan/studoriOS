6cccce07979d9902971d8bcb353c2110003ff522
Omid Keypour <ok455@nyu.edu>
2015-05-06 07:39:29 -0400

    search bar was added to search page

--- Studor/SearchViewController.m
+++ Studor/SearchViewController.m
@@ -13,7 +13,10 @@
 
 @interface SearchViewController ()
 
-@property (retain, nonatomic) NSArray *subjects;
+@property (strong, nonatomic) NSArray *subjects;
+@property (strong, nonatomic) NSArray *searchResults;
+@property (strong, nonatomic) NSMutableArray *subjectString;
+
 @property PFObject *selectedSubject;
 
 @end
@@ -25,11 +28,18 @@
 {
     [super viewDidLoad];
     
+    self.searchResults = [[NSArray alloc] init];
     self.subjects = [[NSMutableArray alloc] init];
     
     PFQuery *subjectQuery = [PFQuery queryWithClassName:@"Subject"];
     self.subjects = [subjectQuery findObjects];
-
+    self.subjectString = [[NSMutableArray alloc] init];
+    for(int x = 0; x < [self.subjects count]; x++){
+        NSLog(_subjects[x][@"subject"]);
+        [self.subjectString addObject:_subjects[x][@"subject"]];
+        
+    }
+    
     
     SWRevealViewController *revealViewController = self.revealViewController;
     if ( revealViewController )
@@ -49,7 +59,13 @@
  numberOfRowsInSection:(NSInteger)section
 {
     
-    return [self.subjects count];
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return [_searchResults count];
+        
+    } else {
+        
+        return [self.subjects count];
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
@@ -58,16 +74,22 @@
     static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:
                              SimpleTableIdentifier];
-
+    
     if (cell == nil) {
         cell = [[UITableViewCell alloc]
                 initWithStyle:UITableViewCellStyleDefault
                 reuseIdentifier:SimpleTableIdentifier];
         cell.accessoryType =  UITableViewCellAccessoryDisclosureIndicator;
-
+        
+    }
+    cell.textLabel.text = nil;
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        cell.textLabel.text = self.searchResults[indexPath.row];
+    }
+    else{
+        cell.textLabel.text = self.subjects[indexPath.row][@"subject"];
     }
     
-    cell.textLabel.text = self.subjects[indexPath.row][@"subject"];
     
     return cell;
 }
@@ -79,18 +101,52 @@
     {
         SearchResultsViewController *destViewController = segue.destinationViewController;
         destViewController.subject = self.selectedSubject;
-
+        
     }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
-    self.selectedSubject = self.subjects[indexPath.row];
-    [self performSegueWithIdentifier:@"searchResultsSegue" sender:self];
-
+    NSString *subjectName = _searchResults[indexPath.row];
+    PFQuery *subjectQuery = [PFQuery queryWithClassName:@"Subject"];
+    [subjectQuery whereKey:@"subject" equalTo:subjectName];
+    self.selectedSubject = [subjectQuery getFirstObject];
     
+    [self performSegueWithIdentifier:@"searchResultsSegue" sender:self];
     
 }
+
+
+
+//size of each cell
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 45;
+}
+//filtering subjects
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"self contains[c] %@", searchText];
+    
+    _searchResults = [_subjectString filteredArrayUsingPredicate:resultPredicate];
+    
+
+}
+
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    NSLog(searchString);
+
+    [self filterContentForSearchText:searchString
+                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                                      objectAtIndex:[self.searchDisplayController.searchBar
+                                                     selectedScopeButtonIndex]]];
+    
+    return YES;
+}
+
+
 
 @end
