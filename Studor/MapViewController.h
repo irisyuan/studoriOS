8d77f92d8eb5db3595553cbd7f0f63cf66f286ca
Marton <marci93@gmail.com>
2015-05-06 08:04:00 -0400

    minor

---
+++ Studor/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Studor
+//
+//  Created by Marton Pono on 5/6/15.
+//  Copyright (c) 2015 Omid Keypour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ Studor/MapViewController.m
@@ -0,0 +1,50 @@
+//
+//  MapViewController.m
+//  Studor
+//
+//  Created by Marton Pono on 5/6/15.
+//  Copyright (c) 2015 Omid Keypour. All rights reserved.
+//
+
+/*#import "MapViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.868
+                                                            longitude:151.2086
+                                                                 zoom:6];
+    GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = camera.target;
+    marker.snippet = @"Hello World";
+    marker.appearAnimation = kGMSMarkerAnimationPop;
+    marker.map = mapView;
+    
+    self.view = mapView;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
